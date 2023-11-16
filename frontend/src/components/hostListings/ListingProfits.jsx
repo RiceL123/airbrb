4801b7d17f140372a50aeb3a197b2b2b6292365c
Eric Liao <ericliao57@gmail.com>
2023-11-16 12:02:10 +1100

    somewhat working graph?

--- frontend/src/components/hostListings/ListingProfits.jsx
+++ frontend/src/components/hostListings/ListingProfits.jsx
@@ -4,20 +4,18 @@ import { Card, CardContent } from '@mui/material';
 import { BarChart } from '@mui/x-charts/BarChart';
 
 const ListingProfits = ({ listings, bookings }) => {
-  // const uData = [25, 60, 90, 10, 3, 0, 0, 10, 90, 200, 200]
-  const xLabels = [...Array(30).keys()];
+  const lastNDays = 30;
+  const today = new Date();
+  const thirtyDaysAgo = new Date();
+  thirtyDaysAgo.setDate(today.getDate() - lastNDays);
+  const _MS_PER_DAY = 1000 * 60 * 60 * 24;
 
-  const profitPerDay = Array(30).fill(0);
+  const xLabels = [...Array(lastNDays).keys()];
+
+  const profitPerDay = Array(lastNDays).fill(0);
 
   // Calculate total profits
   const calculateProfits = () => {
-    const today = new Date();
-    const thirtyDaysAgo = new Date();
-    thirtyDaysAgo.setDate(today.getDate() - 30);
-
-    console.log(`today = ${today}; thirtydaysago = ${thirtyDaysAgo}`);
-    const _MS_PER_DAY = 1000 * 60 * 60 * 24;
-
     let totalProfits = 0;
 
     // Iterate through bookings and sum up total prices
@@ -26,34 +24,27 @@ const ListingProfits = ({ listings, bookings }) => {
       const startDate = new Date(booking.dateRange.startDate);
       const endDate = new Date(booking.dateRange.endDate);
 
-      const listing = listings.find(x => x.id === booking.listingId);
-      console.log(listing);
+      const listing = listings.find(x => `${x.id}` === booking.listingId);
 
-      // only looking at dates in the last 30 days
-      if (startDate > today || endDate <= thirtyDaysAgo) {
+      if (!listing) {
         continue;
       }
 
-      console.log(booking)
       while (startDate.getDate() <= endDate.getDate()) {
-        const utc1 = Date.UTC(startDate.getFullYear(), startDate.getMonth(), startDate.getDate());
-        const utc2 = Date.UTC(thirtyDaysAgo.getFullYear(), thirtyDaysAgo.getMonth(), thirtyDaysAgo.getDate());
+        const utc2 = Date.UTC(startDate.getFullYear(), startDate.getMonth(), startDate.getDate());
+        const utc1 = Date.UTC(thirtyDaysAgo.getFullYear(), thirtyDaysAgo.getMonth(), thirtyDaysAgo.getDate());
 
         const dateDifference = Math.floor((utc2 - utc1) / _MS_PER_DAY);
 
-        console.log(`dateDifference ${dateDifference} of ${startDate} - ${thirtyDaysAgo}`);
-
-        if (dateDifference >= 0 || dateDifference < 30) {
-          profitPerDay[dateDifference] = 5
+        if (dateDifference >= 0 || dateDifference < lastNDays) {
+          profitPerDay[dateDifference] = parseFloat(listing.price);
+          totalProfits += parseFloat(listing.price);
         }
 
         startDate.setDate(startDate.getDate() + 1);
       }
-
-      totalProfits += parseFloat(booking.totalPrice);
     }
 
-    console.log(profitPerDay);
     return totalProfits;
   };
 
@@ -61,10 +52,9 @@ const ListingProfits = ({ listings, bookings }) => {
 
   return (
     <>
-      <Card>
-        <Typography>Profits for past 30 days</Typography>
+      <Card sx={{ p: 1 }}>
+        <Typography>Profits for past {lastNDays} days from {thirtyDaysAgo.toDateString()} to {today.toDateString()}: ${totalProfits}</Typography>
         <CardContent>
-          <Typography>Total Profits: ${totalProfits.toFixed(2)}</Typography>
           <BarChart
             width={500}
             height={300}
